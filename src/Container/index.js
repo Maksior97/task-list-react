0c1e432ff517b7df1ffd8a017db91c686b6339b0
Maksymilian Chrapkowicz <maksymilian.chrapkowicz@gmail.com>
2023-04-05 14:53:34 +0200

    destructurization container

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,8 +1,8 @@
 import "./style.css";
 
-const Container = (props) => (
+const Container = ({children}) => (
     <main className="Container">
-        {props.children}
+        {children}
     </main>
 );
 
